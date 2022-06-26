a8aff1ae017234c200f0fc996982219e1e011526
Sujan <sujantimalsina079@gmail.com>
2022-06-26 15:22:10 +0545

    Manaing Dependency

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from bots.account_create import run as account_create
-from bots.poster import run as poster
-from bots.post_creator import run as post_creator
-from bots.commenter import run as commentor
+from .bots.account_create import run as account_create
+from .bots.poster import run as poster
+from .bots.post_creator import run as post_creator
+from .bots.commenter import run as commentor
 def main():
     #account_create()
     # post_creator()
