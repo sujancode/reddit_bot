be0078a93efc73ede5d406e1f5cc5377dbc58a21
Sujan <sujantimalsina079@gmail.com>
2022-07-06 02:10:44 +0545

    Added Setting for chromium

--- dependency/selenium/Selenium.py
+++ dependency/selenium/Selenium.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+from webdriver_manager.core.utils import ChromeType
 
 def getSeleniumBrowserAutomation():
+    
     option = webdriver.ChromeOptions()
     option.add_argument('--disable-notifications')
     option.add_argument("--mute-audio")
@@ -11,5 +13,5 @@ def getSeleniumBrowserAutomation():
     option.add_argument('--no-sandbox')
     option.add_argument("--disable-dev-shm-usage") 
 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=option)
+    driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(), options=option)
     return driver
