127dc00047928d3212dfd2a470446d0703c29ea3
Sujan <sujantimalsina079@gmail.com>
2022-07-11 00:16:07 +0545

    Made Chaanges to selemium instances

--- dependency/selenium/Selenium.py
+++ dependency/selenium/Selenium.py
@@ -10,9 +10,12 @@ def getSeleniumBrowserAutomation():
     option.add_argument("--mute-audio")
     option.add_argument('--headless')
     option.add_argument('--disable-gpu') 
+    option.add_argument("enable-automation")
+    option.add_argument("--disable-infobars")
+
     option.add_argument('--no-sandbox')
     option.add_argument("--disable-dev-shm-usage") 
-    options.add_argument(f"window-size={random.randint(1000,2000)},{random.randint(1000,2000)}")
+    option.add_argument(f"window-size={random.randint(1000,2000)},{random.randint(1000,2000)}")
 
 
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=option)
